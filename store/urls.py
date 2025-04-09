60bbcd22dd292c7821580dfc2a3c64e432f2c008
RamGavade <ramgavade2004@gmail.com>
2025-04-09 15:22:25 +0530

    Implement search bar

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),  # Home store page
-    path('store/<slug:category_slug>/', views.store, name='products_by_category'),  # Category filter
-    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),  # Product detail
+    path('category/<slug:category_slug>/', views.store, name='products_by_category'),  # Category filter
+    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),  # Product detail
+    path('search/', views.search, name='search'),
+    
 ]
