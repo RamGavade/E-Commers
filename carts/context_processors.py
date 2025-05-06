44e34d6f3386242ed038aaed52e598be76236042
RamGavade <ramgavade2004@gmail.com>
2025-05-06 20:34:06 +0530

    create checkout page

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,16 +1,20 @@
 from .models import Cart, CartItem
 from .views import _cart_id
 
+
 def counter(request):
     cart_count = 0
     if 'admin' in request.path:
         return {}
     else:
         try:
-            cart = Cart.objects.get(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-            cart_count = sum(item.quantity for item in cart_items)
+            cart = Cart.objects.filter(cart_id=_cart_id(request))
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
+            for cart_item in cart_items:
+                cart_count += cart_item.quantity
         except Cart.DoesNotExist:
             cart_count = 0
-
-    return {'cart_count': cart_count}
+    return dict(cart_count=cart_count)
