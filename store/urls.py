6a14de6843a8780395562af5688df72a1164916b
RamGavade <ramgavade2004@gmail.com>
2025-05-06 23:52:56 +0530

    create ratting

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),  # Category filter
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),  # Product detail
     path('search/', views.search, name='search'),
-    
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review'),
 ]
